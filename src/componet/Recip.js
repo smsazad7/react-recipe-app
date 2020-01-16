0b8b741f891b1da228dcee725d9a9e82c4fff0f8
smsazad7 <smsazad7@gmail.com>
2020-01-17 00:03:50 +0300

    all new

--- src/componet/Recip.js
+++ src/componet/Recip.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Recip extends Component {
   render() {
-    return <div>Hello form Recipe</div>;
+    const {
+      publisher,
+      title,
+      source_url,
+      recipe_id,
+      image_url
+    } = this.props.recipe;
+
+    return (
+      <div className="col-10 mx-auto col-md-6 col-lg-4 mt-3">
+        <div className="card" style={{ height: "100%" }}>
+          <img
+            src={image_url}
+            alt="recipe"
+            style={{ height: "17rem" }}
+            className="img-card-top"
+          />
+          <div className="card-body text-capitalize">
+            <h6>{title}</h6>
+            <h6 className="text-warning text-slanted">
+              {" "}
+              provided by {publisher}
+            </h6>
+          </div>
+          <div className="card-footer">
+            <Link
+              to={`/recipes/${recipe_id}`}
+              className="btn btn-primary text-capitalize"
+            >
+              delails
+            </Link>
+            <a
+              href={source_url}
+              className="btn btn-success text-capitalize mx-3"
+            >
+              source url
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/componet/RecipeList.js
+++ src/componet/RecipeList.js
@@ -3,11 +3,24 @@ import Recip from "./Recip";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        Hello form RecipeList
-        <Recip />
-      </div>
+      <>
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mt-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* title end */}
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recip key={recipe.recipe_id} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
